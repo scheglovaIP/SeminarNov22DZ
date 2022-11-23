8bed4e165229c1429f0a22c15d28cb2ac072941c
Irina <ip_scheglova@mail.ru>
2022-11-23 22:14:27 +0500

    add Home Work Number 27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+string ReadInt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+string a = ReadInt("Введите число: ");
+int length = a.Length;
+
+int[] y = new int[length];
+for (int i = 0; i < length; i++)
+{
+    y[i] = Convert.ToInt32(a[i].ToString());
+}
+
+int result = 0;
+for (int i = 0; i < length; i++)
+{
+    result += y[i];
+}
+
+Console.WriteLine(result);
