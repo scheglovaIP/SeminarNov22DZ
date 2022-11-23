85ea30c8e093cd8ee491f30d04acce02093c79d1
Irina <ip_scheglova@mail.ru>
2022-11-23 22:31:58 +0500

    add Home Work Number 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из N элементов 
+//и выводит их на экран. Вывод сделать отдельным методом
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = ReadInt("Введите размер массива: ");
+
+int[] elements = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    elements[i] = new Random().Next(0, 10);
+}
+
+PrintArray(elements);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
