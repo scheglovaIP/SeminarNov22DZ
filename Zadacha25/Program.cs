b734f9148f660c0f7d335804d2960633ce07b3dd
Irina <ip_scheglova@mail.ru>
2022-11-23 12:43:35 +0500

    add Home Work Number 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int a = ReadInt("Введите основание: ");
+
+int b = ReadInt("Введите значение степени: ");
+
+int result = 1;
+for(int i=0; i<b;i++)
+{
+    result*=a;
+}
+
+Console.WriteLine($"число {a} в степени {b} равно {result}");
